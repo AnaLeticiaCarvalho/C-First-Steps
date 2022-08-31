2a1fc7a7d4f9ee4fc190a1beaaade6d80f55cccf
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-31 08:51:47 -0300

    first steps C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Begining
+{
+    class program{
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é o seu nome? "); //Não pula uma linha
+            string nome = Console.ReadLine(); //Recebe a entrada do usuário
+
+            Console.Write("Qual é a sua idade? "); //Não pula uma linha
+            int idade = Convert.ToInt32(Console.ReadLine()); //Recebe a entrada do usuário
+
+            Console.WriteLine("Seu nome é: " + nome + " Sua idade é: " + idade);  //Pula uma linha
+
+            Console.Write("Qual é o seu salário? ");
+            
+
+            
+
+
+        }
+    }
+}
